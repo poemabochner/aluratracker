27e3a03e6b2e663eefbde35f1a8c9a04096cf280
Poema Bochner <poemabochner8@gmail.com>
2024-05-02 21:06:14 -0300

    usando o setInterval e o toISOString para criar o cronômetro

--- src/components/FormularioTarefa.vue
+++ src/components/FormularioTarefa.vue
@@ -4,22 +4,45 @@
       <div class="column is-8" role="form" aria-label="formulário para a criaçao de uma nova tarefa">
         <input type="text" class="text" placeholder="tarefa">
       </div>
-      <div>
-        <section>
-          <strong class="has-text-black">00:00:00</strong>
-        </section>
-        <button class="button">
+      <div class="column">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong class="has-text-black">{{ tempoDecorrido }}</strong>
+          </section>
+          <button class="button" @click="iniciar()">
             <span>play</span>
-        </button>
-        <button class="button">
+          </button>
+          <button class="button" @click="finalizar">
             <span>stop</span>
-        </button>
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'FormularioTarefa'
+  name: 'FormularioTarefa',
+  data() {
+    return {
+      tempoEmSegundos: 0
+    }
+  },
+  computed: {
+    tempoDecorrido () {
+      return new Date(this.tempoEmSegundos*1000).toISOString().substring(19,11);
+    }
+  },
+  methods: {
+    iniciar() {
+      setInterval(() => {
+        this.tempoEmSegundos++
+      }, 1000)
+    },
+
+    finalizar() {
+
+    }
+  }
 }
 </script>
