1377d50231ee9294e13fbc0e1a318296b7598b23
Poema Bochner <poemabochner8@gmail.com>
2024-05-06 20:23:23 -0300

    desabilitando botoes conforme lógica

--- src/components/TemporizadorComponent.vue
+++ src/components/TemporizadorComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <cronometro-component :tempoEmSegundos="tempoEmSegundos"></cronometro-component>
-    <button class="button" @click="iniciar()">
+    <button class="button" @click="iniciar()" :disabled="cronometroRodando">
       <span>play</span>
     </button>
-    <button class="button" @click="finalizar">
+    <button class="button" @click="finalizar" :disabled="!cronometroRodando">
       <span>stop</span>
     </button>
   </div>
@@ -19,17 +19,20 @@ export default {
   data() {
     return {
       tempoEmSegundos: 0,
-      cronometro: 0
+      cronometro: 0,
+      cronometroRodando: false
     }
   },
   methods: {
     iniciar() {
+      this.cronometroRodando = true;
       this.cronometro = setInterval(() => {
         this.tempoEmSegundos++
       }, 1000)
     },
 
     finalizar() {
+      this.cronometroRodando = false;
       clearInterval(this.cronometro);
     }
   }
